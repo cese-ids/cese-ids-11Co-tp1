83499735e7e3a63db12af02375999d1783542512
FELIPE SARCHE <sarche1234@gmail.com>
2020-03-03 23:42:01 -0500

    desde vs test

--- src/alumnos.c
+++ src/alumnos.c
@@ -56,7 +56,7 @@ bool FelipeSarche(char * cadena, size_t espacio) {
     static const struct alumno_s ALUMNO = {
         .apellidos = "SARCHE CARRION",
         .nombres = "Felipe Esteban",
-        .documento = "171959",
+        .documento = "1719591",
     };
     return SerializarAlumno(cadena, espacio, &ALUMNO);
 }
