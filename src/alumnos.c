25d272ba638a4e423899a5ff974c763088e12dd3
felipe sarche <sarche_2004@hotmail.com>
2020-03-06 13:24:09 -0500

    EJERCICIO 1 ALUMNO FELIPE SARCHE

--- src/alumnos.c
+++ src/alumnos.c
@@ -63,7 +63,7 @@ bool FelipeSarche(char * cadena, size_t espacio) {
     static const struct alumno_s ALUMNO = {
         .apellidos = "SARCHE CARRION",
         .nombres = "Felipe Esteban",
-        .documento = "171717",
+        .documento = "171919",
     };
     return SerializarAlumno(cadena, espacio, &ALUMNO);
 }
