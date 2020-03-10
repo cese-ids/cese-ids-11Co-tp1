6219088bfb8ecd3e0285c8a54acea6105ad8d019
FELIPE SARCHE <sarche1234@gmail.com>
2020-03-10 18:05:33 -0500

    Agrego datos alumno Felipe Sarche parte 2

--- inc/alumnos.h
+++ inc/alumnos.h
@@ -9,11 +9,7 @@
 
 /*=====[Avoid multiple inclusion - begin]====================================*/
 
-/**
- *@file alumnos.h
- *@version 1.0.0
- *@brief header file alumnos
- */
+
 
 #ifndef _ALUMNOS_H_
 #define _ALUMNOS_H_
@@ -50,25 +46,9 @@ extern const int CANTIDAD_ALUMNOS;
 /*=====[Prototypes (declarations) of public functions]=======================*/
 
 
-/**
- * @brief FUNCION QUE IMPRIME DATOS BÁSICOS DE UN ALUMNO EN PARTICULAR
- * 
- * @param[in]  cadena      Puntero a espacio de memoria a desplegar en texto
- * @param[in]  espacio     Numero de caracteres 
- * @param[in]  alumno      Estructura con los datos del alumno
- * @return                 Error en la ejecución de la función booleano
- */
 bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno);
 
 
-/**
- * @brief FUNCION QUE IMPRIME DATOS BÁSICOS DE VARIOS ALUMNOS 
- * 
- * @param[in]  cadena      Puntero a espacio de memoria a desplegar en texto
- * @param[in]  espacio     Numero de caracteres 
- * @param[in]  alumnos[]   lista con estructuras de alumnos 
- * @return                 Error en la ejecución de la función booleano
- */
 
 bool SerializarAlumnos(char * cadena, size_t espacio, const alumno_t alumnos[], int cantidad);
 
