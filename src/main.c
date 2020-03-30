f7b22f391654615b798ae1618bf029e3b8b57fc6
felipe sarche <sarche_2004@hotmail.com>
2020-03-30 01:15:17 -0500

    abc

--- src/main.c
+++ src/main.c
@@ -35,19 +35,14 @@
 /*=====[Implementations of private functions]================================*/
 
 int main(int argc, char *argv[]) {
-    char cadena[128];
+    char cadena[256];
 
-    if (AlejandroPermingeat(cadena, sizeof(cadena))) {
-        printf("Alumno: %s\r\n", cadena);
+    if (SerializarAlumnos(cadena, sizeof(cadena), ALUMNOS, CANTIDAD_ALUMNOS)) {
+        printf("Listado total de alumnos: %s\r\n", cadena);
     } else {
-        printf("No se pudo serializar el alumno\r\n");
+        printf("No se pudo serializar un alumno\r\n");
     }
-
-    if (EstebanVolentini(cadena, sizeof(cadena))) {
-        printf("Alumno: %s\r\n", cadena);
-    } else {
-        printf("No se pudo serializar el alumno\r\n");
-    }
+    return 0;
 
     return 0;
 }
